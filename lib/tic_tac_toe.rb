dbeb4dc51629e962e5bad206ef344aa3300dfb4f
Xinran Che <gracexinran@gmail.com>
2019-05-12 05:57:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,6 @@ class TicTacToe
   WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
 
 def display_board()
-  # @board = board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -22,8 +21,8 @@ def input_to_index(user_input)
   end
 end
 
-def move(board, index, char)
-  board[index] = char
+def move(@board, index, char)
+  @board[index] = char
 end
 
 def position_taken?(board, position)
